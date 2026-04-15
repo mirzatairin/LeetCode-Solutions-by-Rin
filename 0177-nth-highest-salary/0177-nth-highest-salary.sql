a06bc7c6deecad9c5bc654275bcc8b7ab50c47dc
Mirza Tairin Tarique <49791777+mirzatairin@users.noreply.github.com>
2026-04-16 00:16:06 +0600

    Time: 513 ms (14.36%), Space: 0B (100%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -1,14 +1,12 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
-    set N = N - 1; 
-    #if i want the 1st highest salary then i would skip 0 rows, for     2nd     highest salary i would skip 1 row etc
   RETURN (
       # Write your MySQL query statement below.
       select distinct salary
-      from employee
-      order by salary desc
-      limit 1 offset N
-
-
+      from (
+        select salary, dense_rank() over (order by salary desc) as rnk
+        from employee
+      ) as subquery
+      where rnk = N
   );
 END
