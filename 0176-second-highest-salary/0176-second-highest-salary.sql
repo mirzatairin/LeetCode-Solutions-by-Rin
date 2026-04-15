9ad81ab261293aefdeda285197dd45670cc49f13
Mirza Tairin Tarique <49791777+mirzatairin@users.noreply.github.com>
2026-04-15 23:50:52 +0600

    Time: 315 ms (12.6%), Space: 0B (100%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select (
+    select distinct salary
+    from Employee
+    order by salary desc # making sure that the salaries are sorted in desc order
+    limit 1 offset 1 # skips the first row and takes the second one
+
+) as SecondHighestSalary; # renaming the output table 
